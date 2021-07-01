636f2107bc495a305d7f78fefc74ae4670b9c677
Mark Cooper <mark.c.cooper@outlook.com>
2021-06-30 17:11:06 -0700

    Rename job helper arg

--- test/job_helper.rb
+++ test/job_helper.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 module JobHelper
-  def attach_spreadsheet(process, spreadsheet)
-    process.batch.spreadsheet.attach(
+  def attach_spreadsheet(step, spreadsheet)
+    step.batch.spreadsheet.attach(
       io: File.open(Rails.root.join('test', 'fixtures', 'files', spreadsheet)),
       filename: spreadsheet,
       content_type: 'text/csv'
     )
   end
 
-  def attach_mapper(process, mapper)
-    process.batch.mapper.config.attach(
+  def attach_mapper(step, mapper)
+    step.batch.mapper.config.attach(
       io: File.open(Rails.root.join('test', 'fixtures', 'files', mapper)),
       filename: mapper,
       content_type: 'application/json'
