f494760a6a07c628f07cec741466d4e558bb5f4f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-07-15 11:54:53 -0400

    implement delete transfer

--- app/services/record_transfer_service.rb
+++ app/services/record_transfer_service.rb
@@ -31,7 +31,7 @@ class RecordTransferService
   def transfer_record(data)
     checker = RecordActionChecker.new(data['status'], transfer_step)
     if checker.deleteable?
-      result = delete_transfer
+      result = delete_transfer(data)
     elsif checker.createable?
       result = create_transfer(data)
     elsif checker.updateable?
@@ -44,6 +44,7 @@ class RecordTransferService
 
   private
 
+  # TODO: make class instance variable?
   def service_path
     if @subtype
       client.service(type: type, subtype: subtype)[:path]
@@ -52,8 +53,25 @@ class RecordTransferService
     end
   end
 
-  def delete_transfer
-    TransferStatus.new(message: 'Delete transfer is not yet implemented')
+  def delete_transfer(data)
+    status = TransferStatus.new
+    rec_id = data['id']
+    uri = data['uri']
+    Rails.logger.debug("Deleting record with ID #{rec_id} at path: #{uri}")
+    begin
+      delete = client.delete(uri)
+      if delete.result.success?
+        status.good("Deleted #{rec_id}")
+        status.set_uri(uri)
+        status.set_action('Deleted')
+        status
+      else
+        status.bad("ERROR: Client response: #{delete.result.body}")
+      end
+    rescue StandardError => e
+      status.bad("ERROR: Transfer error: #{e.message} at #{e.backtrace.first}")
+    end
+    status
   end
 
   def create_transfer(data)
