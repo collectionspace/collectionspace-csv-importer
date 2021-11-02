2fb9318c1cee36bdf8037649aa29d29fb9a90bf8
Mark Cooper <mark.c.cooper@outlook.com>
2021-11-01 22:18:59 -0700

    Less docker cfg

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [[ -z "${SIDEKIQ_ENABLED}" ]]; then
-  ./bin/rails assets:precompile
-  ./bin/rails db:create
-  ./bin/rails db:migrate
-  ./bin/rails db:seed
-fi
+./bin/rails assets:precompile
 exec "$@"
