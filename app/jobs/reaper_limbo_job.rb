9f86bdcbf0986ee1ef61d30db6d274d080a55c1a
Mark Cooper <mark.c.cooper@outlook.com>
2022-06-24 14:44:44 -0700

    Apply record lock for limbo job updates

--- app/jobs/reaper_limbo_job.rb
+++ app/jobs/reaper_limbo_job.rb
@@ -5,31 +5,34 @@ class ReaperLimboJob < ApplicationJob
   sidekiq_options retry: false
 
   def perform
-    # TODO:
-    # - running concurrently would need lock
-    # - may need to optimize (in batches)
+    # TODO: in batches
     Batch.where(status_state: 'running').all.each do |batch|
       next if batch.job_is_active?
 
       step = batch.step
       next unless step.limbo?
 
-      if step.all_rows_accessed? && !step.errors?
-        step.batch.finished!
-        step.update(done: true)
-      else
-        step.batch.failed!
-        step.update(messages: step.messages.append(I18n.t('batch.step.catastrophe')))
+      step.with_lock do
+        if step.all_rows_accessed? && !step.errors?
+          step.batch.finished!
+          step.update(done: true)
+        else
+          step.batch.failed!
+          step.update(messages: step.messages.append(I18n.t('batch.step.catastrophe')))
+        end
+
+        if step.reports.count.zero?
+          step.update(messages: step.messages.append(I18n.t('batch.step.no_reports')))
+        end
+
+        step.update(completed_at: Time.now.utc)
+        step.update_header
+        step.update_progress(force: true)
       end
 
-      if step.reports.count.zero?
-        step.update(messages: step.messages.append(I18n.t('batch.step.no_reports')))
+      batch.with_lock do
+        batch.update(job_id: nil)
       end
-
-      step.update(completed_at: Time.now.utc)
-      step.update_header
-      step.update_progress(force: true)
-      batch.update(job_id: nil)
     end
   end
 end
